7a0c29b3189abfbb814a9c308788de16ba39128c
TRENER322 <solo.dread1@gmail.com>
2026-08-03 20:40:45 +0300

    се проверки пройдены: тесты, покрытие, flake8, mypy, isort

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Any, cast
+from typing import Any, Dict, List, cast
 
 
 def filter_by_state(transactions: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
